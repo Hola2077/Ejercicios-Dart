5039a4a78a68c5601dc3e64726c19fbfd6c9e5bc
NN <113859878+Hola2077@users.noreply.github.com>
2024-05-25 18:49:48 -0500

    Update main.dart

--- Ejercicio_005/main.dart
+++ Ejercicio_005/main.dart
@@ -4,6 +4,9 @@ void main() {
   imprimirMensaje(
     (a) => "$a ${DateTime.now().weekday} dela semana",
   );
+
+  imprimirResultado(
+    (a, b) => a+b); // esto es una funcion anonima
 }
 
 void imprimirMensaje(String Function(String) callback) {
@@ -11,3 +14,7 @@ void imprimirMensaje(String Function(String) callback) {
     callback("Hoy es el Dia"),
   );
 }
+
+void imprimirResultado(int Function(int a,int b) callback){
+  print("la suma es: ${callback(2,5)}");
+} // esto es un callback 
